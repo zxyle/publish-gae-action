e552ff52531d8980b346ec25bb3ca1b26c16b739
Zheng <zxyful@gmail.com>
2019-12-18 13:14:44 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ export serviceAccountEmail="$1"
 export tmpKeyFilePath="client-secret.json"
 
 pwd=$(base64 -d <<<"$2")
-echo "$pwd" >>"client-secret.json"
+echo "$pwd" >>"/github/workspace/client-secret.json"
 
 gcloud config set project "$3"
 
-gcloud auth activate-service-account "$1" --key-file="client-secret.json"
+gcloud auth activate-service-account "$1" --key-file="/github/workspace/client-secret.json"
 
 
 pwd
