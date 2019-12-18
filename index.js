a2df53e7dafe04c084164059757fac604663f6c4
zxyle <zxyful@gmail.com>
2019-12-18 16:04:19 +0800

    update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const core = require('@actions/core');
 try {
     const service_account_key = core.getInput('service_account_key');
     const buf = Buffer.from(service_account_key, 'base64');
-    console.log(buf.toString());
 
     fs.writeFile('./client-secret.json', buf.toString(), function (err) {
         if (err) {
