ea3f15a571aeb163cf1cb5700e47ab77f896a3be
zxyle <zxyful@gmail.com>
2019-12-18 12:43:12 +0800

    Set GCP project id

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,6 @@ export tmpKeyFilePath="client-secret.json"
 pwd=$(base64 -d <<<"$2")
 echo "$pwd" >>"client-secret.json"
 
+gcloud config set project "$3"
+
 gcloud auth activate-service-account "$1" --key-file="client-secret.json"
