a7ec9b85f8cb515e7edf542a375bdb581d64e845
Bernard Nandwa` <robot@Bernards-MacBook-Pro.local>
2020-01-27 16:00:50 +0300

    Update app config using secret variables

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const fs = require('fs');
 const core = require('@actions/core');
+const yaml = require('js-yaml');
+const deepmerge = require('deepmerge');
+
+/**
+ * Add support for updating environment variables with actions secrets
+ *
+ */
+try {
+    const fileContents = fs.readFileSync('./app.yaml', 'utf8');
+
+    let data = yaml.safeLoad(fileContents);
+
+    // Get the environment variables if they exists
+    // @todo Only run this if the user wants to or has secret variables
+    const secrets = core.getInput('gae_variables');
+    const secrets_buffer = Buffer.from(secrets, 'base64');
+
+    data = deepmerge(data, JSON.parse(secrets_buffer.toString()));
+
+    let yamlStr = yaml.safeDump(data);
+
+    fs.writeFileSync('app.yaml', yamlStr, 'utf8');
+} catch (error) {
+    core.setFailed(error.message);
+}
 
 try {
     const service_account_key = core.getInput('service_account_key');
